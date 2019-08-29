ba1c2a0e0abb2a559163fe0ac60d7fe9f8ac9df4
madcido <fabowow@gmail.com>
2019-08-29 10:09:26 -0300

    Responsive searchbar

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -5,10 +5,12 @@ function Searchbar() {
   return (
     <form className="searchbar row center justify-end">
       <input type="text" name="search-text" placeholder="Search by skills" />
-      <input type="radio" name="search-type" value="people" id="radio-people" />
-      <label htmlFor="radio-people">People</label>
-      <input type="radio" name="search-type" value="work" id="radio-work" />
-      <label htmlFor="radio-work">Work</label>
+      <div className='row center'>
+        <input type="radio" name="search-type" value="people" id="radio-people" />
+        <label htmlFor="radio-people">People</label>
+        <input type="radio" name="search-type" value="work" id="radio-work" />
+        <label htmlFor="radio-work">Work</label>
+      </div>
       <input type="submit" value="SEARCH" />
     </form>
   );
